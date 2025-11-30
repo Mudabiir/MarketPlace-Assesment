a35baceec90ee1bae89a386a37ca39765fda766a
Mudabiir <lateefmudabir22@gmail.com>
2025-11-30 21:00:07 +0530

    pagination added

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,10 +1,9 @@
 import jwt from "jsonwebtoken";
 
 export function authenticateToken(req, res, next) {
-  const token = req.cookies?.accesstoken || req.header("Authorization")?.replace("Bearer", "")
+  const token = req.cookies?.token || req.header("Authorization")?.replace("Bearer", "")
   if (!token) return res.sendStatus(401);
   console.log(token);
-
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
